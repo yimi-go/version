d81036be4efda8ccab490688f4abada7c24c6905
dowenliu-xyz <hawkdowen@hotmail.com>
2023-04-08 17:39:54 +0800

    build: shellcheck

--- build/scripts/check-is-break-time.sh
+++ build/scripts/check-is-break-time.sh
@@ -6,19 +6,19 @@ function in_time_range()
 {
     cur_ts=$(date -j "+%s")
     time_range=$1
-    start_day_time=$(echo $time_range | awk -F '-' '{print $1}')
-    end_day_time=$(echo $time_range | awk -F '-' '{print $2}')
-    start_time=$(date -j "+%Y-%m-%d "$start_day_time)
-    end_time=$(date -j "+%Y-%m-%d "$end_day_time)
+    start_day_time=$(echo "$time_range" | awk -F '-' '{print $1}')
+    end_day_time=$(echo "$time_range" | awk -F '-' '{print $2}')
+    start_time=$(date -j "+%Y-%m-%d ""$start_day_time")
+    end_time=$(date -j "+%Y-%m-%d ""$end_day_time")
     start_ts=$(date -j -f "%Y-%m-%d %H:%M:%S" "$start_time" "+%s")
     end_ts=$(date -j -f "%Y-%m-%d %H:%M:%S" "$end_time" "+%s")
-    if [ $start_ts -lt $end_ts ]; then
-        if [ $cur_ts -gt $start_ts -a $cur_ts -lt $end_ts ]; then
+    if [ "$start_ts" -lt "$end_ts" ]; then
+        if [ "$cur_ts" -gt "$start_ts" ] && [ "$cur_ts" -lt "$end_ts" ]; then
             return 1
         fi
     fi
-    if [ $start_ts -gt $end_ts ]; then
-        if [ $cur_ts -lt $end_ts -o $cur_ts -gt $start_ts ]; then
+    if [ "$start_ts" -gt "$end_ts" ]; then
+        if [ "$cur_ts" -lt "$end_ts" ] || [ "$cur_ts" -gt "$start_ts" ]; then
             return 1
         fi
     fi
@@ -26,12 +26,12 @@ function in_time_range()
 }
 
 weekday=$(date -j "+%w")
-if [ $weekday -eq 0 -o $weekday -eq 6 ]; then
+if [ "$weekday" -eq 0 ] || [ "$weekday" -eq 6 ]; then
   exit 0
 fi
 
-for break_time in ${break_times[@]}; do
-    in_time_range $break_time
+for break_time in "${break_times[@]}"; do
+    in_time_range "$break_time"
     if [ $? -eq 1 ]; then
       exit 0
     fi
--- build/scripts/ensure_tag.sh
+++ build/scripts/ensure_tag.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2153
 version="${VERSION}"
 if [ "${version}" == "" ];then
-  version=v`gsemver bump`
+  version=v$(gsemver bump)
 fi
 
-if [ -z "`git tag -l ${version}`" ];then
-  git tag -a -m "release version ${version}" ${version}
+if [ -z "$(git tag -l "${version}")" ];then
+  git tag -a -m "release version ${version}" "${version}"
 fi
